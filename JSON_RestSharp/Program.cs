5b90a307f18ab5feea7d84237484829d9cec0f40
Prajwal <prajwal.zurunge12@gmail.com>
2021-11-21 16:00:36 +0530

    first commit

---
+++ JSON_RestSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSON_RestSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to json server an rest sharp program");
+        }
+    }
+}
